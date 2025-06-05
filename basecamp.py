c6da539f2c7f2813fdbf4f2721516ff671b5dc53
Uttam Shreyakar <39872039+uttamds@users.noreply.github.com>
2025-06-05 15:07:25 +0530

    Update basecamp.py

--- basecamp.py
+++ basecamp.py
@@ -17,3 +17,23 @@ if student_record['age'] <18:
     print("The student is a minor")
 else:
     print("Student is adult")
+
+
+#======================================================
+cgpa = []
+cutoff = 0.0
+print("Enter cgpa of 10 students")
+for i in range(10):
+    print(f"Student {i+1} cgpa")
+    cgpa.append(float(input()))
+print("Enter the cut-off cgpa")
+cutoff = float(input())
+
+#list comprehension
+qualified  = [m for m in cgpa if m >=cutoff]
+toppers  = [m for m in cgpa if m >=8.8]
+
+
+print("Qualified list of cgpa")
+print(qualified)
+print(toppers)
